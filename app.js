e236c033b05c29bc3aa8fbed976e4f2301d39e23
sol <wlsthf1113@gmail.com>
2021-12-14 21:31:31 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const PORT = 8080;
+const app = express();
+app.get('/', (req, res) => {
+  res.send('hello, world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening at ${PORT}`);
+});
