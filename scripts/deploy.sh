ed738be54b3490de4320699f4fea38fcf15c642f
soljjj <wlsthf1113@gmail.com>
2021-12-15 16:01:11 +0900

    ec2-user 환경설정 적용

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-/home/ec2-user/.nvm/versions/node/v14.18.2/bin/pm2 delete all
+source /home/ec2-user/.bash_profile
+
+pm2 delete all
 cd /home/ec2-user/build/
-/home/ec2-user/.nvm/versions/node/v14.18.2/bin/pm2 start app.js
+pm2 start app.js
